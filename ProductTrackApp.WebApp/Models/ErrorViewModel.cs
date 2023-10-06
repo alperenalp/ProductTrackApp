01b3d27efd1e7d61f769815a7cc9c28a7b76ef2f
alperenalp <alperen.alp045@gmail.com>
2023-10-06 22:52:51 +0300

    Added MVC presentation layer and added AdminLTE 3.0.4 theme

---
+++ ProductTrackApp.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductTrackApp.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
